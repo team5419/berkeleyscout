f946d86faa4e036e546489dcee0ac0a9318c0ecf
FelixMo42 <felix@tacocat.com>
2020-03-05 21:00:22 -0800

    got qr scaner working

--- browser/index.js
+++ browser/index.js
@@ -1,9 +1,3 @@
-var authorizeButton = document.getElementById('authorize_button')
-authorizeButton.onclick = () => gapi.auth2.getAuthInstance().signIn()
-
-var signoutButton = document.getElementById('signout_button')
-signoutButton.onclick = () => gapi.auth2.getAuthInstance().signOut()
-
 // load the api
 gapi.load('client:auth2', () => gapi.client.init({
     apiKey: 'AIzaSyBD9wsKIBZnvoOepvj901KJGfaf-mD_fjc',
@@ -11,6 +5,14 @@ gapi.load('client:auth2', () => gapi.client.init({
     discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
     scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
 }).then(() => {
+    // set up the sign in button
+    var authorizeButton = document.getElementById('authorize_button')
+    authorizeButton.onclick = () => gapi.auth2.getAuthInstance().signIn()
+
+    // set up the sign out button
+    var signoutButton = document.getElementById('signout_button')
+    signoutButton.onclick = () => gapi.auth2.getAuthInstance().signOut()
+
     // a callback for when the sign-in state changes.
     let onSignInStatusChanges = isSignedIn => isSignedIn ? onSignin() : onSignout()
 
@@ -21,29 +23,84 @@ gapi.load('client:auth2', () => gapi.client.init({
     onSignInStatusChanges(gapi.auth2.getAuthInstance().isSignedIn.get())
 }).catch(console.error))
 
+// get wether were signed in or not
 let isSignedIn = () => gapi.auth2.getAuthInstance().isSignedIn.get()
 
+// get refrence to elements needed for camera
+const stream = document.getElementById("stream")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext('2d')
+
+// link the camera to the stream
+navigator.mediaDevices.getUserMedia({ video: true }).then(camera => {
+    stream.srcObject = camera
+    stream.play()
+}).catch(console.error)
+
+// scan the image to see if there is a qr code in view
+function scan() {
+    // get width and height of image
+    let width  = Math.floor(stream.videoWidth)
+    let height = Math.floor(stream.videoHeight)
+
+    // make all the widths equal so it dosent crop the image
+    canvas.width = ctx.width = width
+    canvas.height = ctx.height = height
+
+    // if they are zero then its not ready yet
+    if (width == 0 || height == 0) return null
+
+    // draw the image on to the canvas
+    ctx.drawImage(stream, 0, 0, width, height)
+
+    // then load if back to get it as an ImageData
+    let imageData = ctx.getImageData(0, 0, width, height)
+
+    // do the scan
+    let data = jsQR(imageData.data, imageData.width, imageData.height)
+
+    if (data !== null) return data.data
+}
+
+// even how many ms should we scan?
+const scanInterval = 10
+
+// scan the image repeatidly
+setInterval(() => {
+    // we dont even want to scan if were not logged in
+    if (!isSignedIn()) return
+
+    // get the data from the scan
+    let data = scan()
+    if (data) onData(data)
+}, scanInterval)
+
+// a list of all games we have stored up
+let games = []
+
+// what should we do when we get a successful qr code?
+function onData(data) {
+    
+}
+
+// what should we do when we sign in
 function onSignin() {
     authorizeButton.style.display = 'none'
     signoutButton.style.display = 'block'
-
-    doStuff()
 }
 
+// what should we do when we sign out
 function onSignout() {
     authorizeButton.style.display = 'block'
     signoutButton.style.display = 'none'
 }
 
-function doStuff() {
-    gapi.client.sheets.spreadsheets.values.update({
-        spreadsheetId: '1Nc7NNZmOd75HYn5kJ6SyO9mh1Uxw6zCXJV_UwfEhpSo',
+// upload a match to sheets
+function uploadMatch() {
+    return gapi.client.sheets.spreadsheets.values.update({
+        spreadsheetId: "1Nc7NNZmOd75HYn5kJ6SyO9mh1Uxw6zCXJV_UwfEhpSo",
         range: 'Sheet1!A1:A1',
         valueInputOption: "USER_ENTERED",
         resource: {values: [["blablabla7"]]}
-    }).then(response => {
-        
-    }).catch(err => {
-        console.error(err)
     })
 }
